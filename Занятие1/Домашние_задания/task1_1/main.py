f458ec49f28881c038bd3d23ce892f882b01e52c
@arkivanov <100686042+arkadiyivanov@users.noreply.github.com>
2022-04-26 11:00:52 +0300

    Занятие 2

--- Занятие1/Домашние_задания/task1_1/main.py
+++ Занятие1/Домашние_задания/task1_1/main.py
@@ -1,5 +1,9 @@
 
+numbers = ["zero", "two", "four", "six"]
 
+for index, item in zip(range(0, 7, 2), numbers):
+
+    print (index, item)
 if __name__ == "__main__":
     # Write your solution here
     pass

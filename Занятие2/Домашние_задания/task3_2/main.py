eb2b4778c614420688c7306924f86741857c7274
@arkivanov <100686042+arkadiyivanov@users.noreply.github.com>
2022-04-28 15:41:07 +0300

    Занятие 3

--- Занятие2/Домашние_задания/task3_2/main.py
+++ Занятие2/Домашние_задания/task3_2/main.py
@@ -1,3 +1,17 @@
+import functools
+def decorator(*args):
+    def wrapper(*args):
+        if args== int:
+            print("Аргументы являются INT")
+    return wrapper
+
+@decorator
+def f(*args):
+    args
+    print(f)
+# Применяем декоратор с параметрами
+
+
 if __name__ == "__main__":
     # Write your solution here
     pass

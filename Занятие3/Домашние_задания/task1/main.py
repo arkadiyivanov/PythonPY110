200ce03d484059cad4b14eab904b9c1c43e34f5c
@arkivanov <100686042+arkadiyivanov@users.noreply.github.com>
2022-05-06 14:12:33 +0300

    Занятие 3

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -1,17 +1,18 @@
-
+LIST_FILE_FINAL = 'listfile3.txt'
+LIST_FILE = 'listfile.txt'
+LIST_F ='list_f.txt'
 def task():
     places = ['Berlin', 'Cape Town', 'Sydney', 'Moscow']
     with open('listfile.txt', 'x') as listfile1:
         for listitem in places:
-            f.write('%s\n' % listitem)
+            listfile1.write('%s\n' % listitem)
     listfile1.close()
     places = ['100', '200', '300', '400']
     with open('list_f.txt', 'x') as f:
         for listitem in places:
             f.write('%s\n' % listitem)
     f.close()
-    LIST_FILE = 'listfile.txt'
-    LIST_F ='list_f.txt'
+
     with open('listfile3.txt', "x") as listfile3:
         with open(LIST_FILE, 'r') as listfile:
             for f in listfile:
@@ -19,10 +20,11 @@ def task():
         with open(LIST_F, 'r') as listfile:
             for f in listfile:
                 listfile3.write(f)
-
     listfile3.close()
+
 if __name__ == "__main__":
-     task()
-
-
+    task()
+    with open(LIST_FILE_FINAL) as file:
+        for line in file:
+            print(line, end="")
 

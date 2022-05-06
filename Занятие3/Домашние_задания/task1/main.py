8acba301268fa14b3697ecb9563b85041c441d97
@arkivanov <100686042+arkadiyivanov@users.noreply.github.com>
2022-05-06 14:01:25 +0300

    Занятие 3

--- Занятие3/Домашние_задания/task1/main.py
+++ Занятие3/Домашние_задания/task1/main.py
@@ -1,22 +1,28 @@
 
 def task():
     places = ['Berlin', 'Cape Town', 'Sydney', 'Moscow']
-    with open('listfile.txt', 'x') as filehandle:
+    with open('listfile.txt', 'x') as listfile1:
         for listitem in places:
-            filehandle.write('%s\n' % listitem)
-    filehandle.close()
+            f.write('%s\n' % listitem)
+    listfile1.close()
     places = ['100', '200', '300', '400']
     with open('list_f.txt', 'x') as f:
         for listitem in places:
             f.write('%s\n' % listitem)
     f.close()
-def task_3():
-    with open(listfile, "r") as input_file:
-        with open(OUTPUT_FILE, "w") as output_file:
-            for upper_line in map(str.upper, input_file):
-                output_file.write(upper_line)
+    LIST_FILE = 'listfile.txt'
+    LIST_F ='list_f.txt'
+    with open('listfile3.txt', "x") as listfile3:
+        with open(LIST_FILE, 'r') as listfile:
+            for f in listfile:
+                listfile3.write(f)
+        with open(LIST_F, 'r') as listfile:
+            for f in listfile:
+                listfile3.write(f)
 
+    listfile3.close()
 if __name__ == "__main__":
+     task()
 
 
 
